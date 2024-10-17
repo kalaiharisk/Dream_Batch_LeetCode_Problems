8ce4c810cff64bf9face5c2402de0381dc454a49
HARIHARAN S <125335634+kalaiharisk@users.noreply.github.com>
2024-10-17 09:27:24 +0530

    Time: 3 ms (38.18%) | Memory: 7.8 MB (12.29%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s=to_string(num);
+        string t=s;
+        sort(s.rbegin(),s.rend());
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!=t[i])
+            {
+                int ind=-1;
+                for(int j=0;j<t.size();j++)
+                {
+                    if(t[j]==s[i])
+                    {
+                        ind=j;
+                    }
+                }
+                swap(t[ind],t[i]);
+                break;
+            }
+
+        }
+        return stoi(t);
+    }
+};
