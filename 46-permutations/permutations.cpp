ea83362beb3914b7baccef5373b826ec1b53a098
HARIHARAN S <125335634+kalaiharisk@users.noreply.github.com>
2024-10-21 12:13:00 +0530

    Time: 3 ms (52.59%) | Memory: 9.1 MB (87.05%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    void backtrack(vector<int>& nums,vector<vector<int>>& result,int start)
+    {
+        if(start==nums.size())
+        {
+            result.push_back(nums);
+            return;
+        }
+        for(int i=start;i<nums.size();i++)
+        {
+            swap(nums[start],nums[i]);
+            backtrack(nums,result,start+1);
+            swap(nums[start],nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(nums,result,0);
+        return result;
+        
+    }
+};
