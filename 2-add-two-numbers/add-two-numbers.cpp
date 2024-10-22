72a95ec8c2091c96cba474fdc05c79a8f01148bb
HARIHARAN S <125335634+kalaiharisk@users.noreply.github.com>
2024-10-22 13:54:09 +0530

    Time: 4 ms (96.49%) | Memory: 77.5 MB (41.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -14,23 +14,19 @@ public:
         ListNode* dummyhead=new ListNode();
         ListNode* current=dummyhead;
         int carry=0;
-
-        while(l1!=nullptr || l2!=nullptr)
+        while(l1!=nullptr || l2!=nullptr || carry>0)
         {
-            int x=(l1!=nullptr)?l1->val:0;
-            int y=(l2!=nullptr)?l2->val:0;
-            int sum=x+y+carry;
+            int val1=(l1!=nullptr)?l1->val:0;
+            int val2=(l2!=nullptr)?l2->val:0;
+            int sum=val1+val2+carry;
             carry=sum/10;
-            current->next=new ListNode(sum%10);
-
+            int digit=sum%10;
+            current->next=new ListNode(digit);
             current=current->next;
-            if(l1!=nullptr) l1=l1->next;
-            if(l2!=nullptr) l2=l2->next;
 
-        }
-        if(carry>0)
-        {
-            current->next=new ListNode(carry);
+            if(l1!=nullptr)  l1=l1->next;
+            if(l2!=nullptr)  l2=l2->next;
+
         }
         return dummyhead->next;
         
