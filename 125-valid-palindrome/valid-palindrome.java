a20d202257d6f7cc0248481b8fb666ef38831a1c
HARIHARAN S <125335634+kalaiharisk@users.noreply.github.com>
2024-10-24 14:27:13 +0530

    Time: 4 ms (55.35%) | Memory: 42.8 MB (83.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb=new StringBuilder ();
-        for(char c: s.toCharArray())
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray())
         {
             if(Character.isLetterOrDigit(c))
             {
@@ -9,19 +9,17 @@ class Solution {
             }
         }
         int l=0;
-        int r=sb.length()- 1;
+        int r=sb.length()-1;
         while(l<r)
         {
             if(sb.charAt(l)!=sb.charAt(r))
             {
                 return false;
             }
-            r--;
             l++;
+            r--;
         }
         return true;
-
-
         
     }
 }
