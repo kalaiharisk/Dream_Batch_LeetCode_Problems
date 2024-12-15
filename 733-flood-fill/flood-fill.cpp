6d0b28562365917f7c5a90c893df8a87b78272c5
HARIHARAN S <125335634+kalaiharisk@users.noreply.github.com>
2024-12-15 20:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (29.59%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int newc) {
-            if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=newc)
-            {
-                return;
-            }
-            image[sr][sc]=color;
-            dfs(image,sr+1,sc,color,newc);
-            dfs(image,sr-1,sc,color,newc);
-            dfs(image,sr,sc+1,color,newc);
-            dfs(image,sr,sc-1,color,newc);
+     void dfs(vector<vector<int>>& image,int sr,int sc,int color,int newc)
+     {
+        if(sr<0 || sr>=image.size() ||sc<0 || sc>=image[0].size()|| image[sr][sc]!=newc){
+            return ;
         }
-
+        image[sr][sc]=color;
+        dfs(image,sr+1,sc,color,newc);
+        dfs(image,sr-1,sc,color,newc);
+        dfs(image,sr,sc+1,color,newc);
+        dfs(image,sr,sc-1,color,newc);
+     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       int newc=image[sr][sc];
-       if(newc!=color)
-       {
-
-       dfs(image,sr,sc,color,newc);
-       }
-       return image;
+        int newc=image[sr][sc];
+        if(newc!=color)
+        {
+            dfs(image,sr,sc,color,newc);
+        }
+        return image;
+        
     }
 };
